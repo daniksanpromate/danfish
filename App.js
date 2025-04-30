9ac3c3a8d1a003c1c163f829cfdfff6cdefba4d6
daniksanpromate <danielsanko09@gmail.com>
2025-04-30 21:47:42 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from "./components/SplashScreen";
+import Home from "./components/Home";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    height: '100%'
+  },
+});
+
+export default function App() {
+  const [isPending, setIsPending] = useState(true);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {isPending ? <SplashScreen setIsPending={setIsPending}/> : <Home/>}
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}

7783474e0821adf9da7ef087ccf37abec8ba6d4e
daniksanpromate <danielsanko09@gmail.com>
2025-05-07 22:39:44 +0300

    Update Right.js,added button

--- components/header/Right.js
+++ components/header/Right.js
@@ -1,27 +1,30 @@
-import { Pressable, Text, View } from 'react-native';
+import {  Pressable, Text, View } from 'react-native';
 import { useState } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+import { Button } from './headerrightButton.js';
 
 export default function Right({ headerRightStyle }) {
-  
-    const [color, setColor] = useState('blue');
-    const [size, setSize] = useState(50);
-
-    const click = () => {
-      color === '#00008B' ? setColor('red') : setColor('#00008B');
-      size === 50 ? setSize(70) : setSize(50)
-    };
-
-    
-
-    
-  
+  const [rightState, setRightState] = useState({
+    color: '#00008B',
+    size: 50,
+  });
+  const click = () => {
+    rightState.color === '#00008B'
+      ? setRightState({
+          color: 'red',
+          size: 70,
+        })
+      : setRightState({
+          color: '#00008B',
+          size: 50,
+        });
+  };
 
   return (
     <View style={headerRightStyle}>
-      <Pressable onPress={click}>
-        <Icon name="settings" size={size} color={color} />
-      </Pressable>
+      <Button action={click}>
+        <Icon name="settings" size={rightState.size} color={rightState.color} />
+      </ Button>
     </View>
   );
 }
